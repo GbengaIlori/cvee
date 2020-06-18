0c237e2c719783acb9e4bde365ad43d5d03952a4
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-18 20:20:00 +0100

    Implement editing for work experience section

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ var sections = []sect.Section{
 	&sect.Header{},
 	&sect.Summary{},
 	&sect.Skills{},
+	&sect.Experience{},
 }
 
 var (
--- section/experience.go
+++ section/experience.go
@@ -1,1 +1,60 @@
 package section
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Experience struct {
+	data   map[string]string
+	points []string
+}
+
+func (e *Experience) init() {
+	//Init data map if not done already
+	if e.data == nil {
+		e.data = map[string]string{
+			"Job Title":         "",
+			"Work Place":        "",
+			"Years (xxxx-xxxx)": "",
+		}
+	}
+}
+
+//Edit allows a user edit this section
+func (e *Experience) Edit() error {
+	e.init()
+	for key := range e.data {
+		fmt.Print(key + ": ")
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			return err
+		}
+		e.data[key] = line
+	}
+
+	// populate points
+	if err := e.editPoints(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Experience) editPoints() error {
+	for {
+		fmt.Print("Notable events / What you did (Empty string to stop): ")
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			return err
+		}
+		if strings.TrimSpace(line) == "" {
+			break
+		}
+		e.points = append(e.points, line)
+	}
+
+	fmt.Println(e.points)
+	return nil
+}
