d63e5e42f9f1e839ebd37936f70b47f2f10663f4
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-18 19:42:05 +0100

    Implement edit in summary section

--- section/summary.go
+++ section/summary.go
@@ -1,1 +1,24 @@
 package section
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Summary struct {
+	summary string
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func (s *Summary) Edit() error {
+	fmt.Print("Resume Objective / Summary: ")
+	lines, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	s.summary = lines
+	fmt.Println(s.summary)
+	return nil
+}
