aade61c2ad0f8322dc2d5c863a9f520af0f6cc46
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-18 19:00:34 +0100

    Get title from user in header

--- section/header.go
+++ section/header.go
@@ -20,12 +20,21 @@ var reader = bufio.NewReader(os.Stdin)
 //Edit allows a user edit this section
 func (h *Header) Edit() error {
 	// get name
-	fmt.Print("Full name: ")
+	fmt.Print("Your full name: ")
 	fname, err := reader.ReadString('\n')
 	if err != nil {
 		return err
 	}
 	h.Name = fname
-	fmt.Println(h.Name)
+
+	// get title
+	fmt.Print("Your title, e.g. Software Engineer: ")
+	title, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	h.Title = title
+
+	fmt.Println(h)
 	return nil
 }
