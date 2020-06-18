351d44e78e5467aacf5a3defa8c790d0a4a2eaa7
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-18 20:48:21 +0100

    Implement editing for education section

--- section/education.go
+++ section/education.go
@@ -1,1 +1,33 @@
 package section
+
+type Education struct {
+	data       map[string]string
+	coursework []string
+}
+
+func (e *Education) init() {
+	//Init data map if not done already
+	if e.data == nil {
+		e.data = map[string]string{
+			"Degree":            "",
+			"School":            "",
+			"Years (xxxx-xxxx)": "",
+			"CGPA":              "",
+		}
+	}
+}
+
+//Edit allows a user edit this section
+func (e *Education) Edit() error {
+	e.init()
+	if err := populateMap(&e.data); err != nil {
+		return err
+	}
+
+	// populate coursework
+	err := populateSlice("Relevant Coursework (Empty string to stop): ", &e.coursework)
+	if err != nil {
+		return err
+	}
+	return nil
+}
